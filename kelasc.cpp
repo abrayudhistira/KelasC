df21eade8fc662eb0ae13eba1b9a07d4ded37b46
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-03-04 19:40:29 +0700

    first commit

---
+++ kelasc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//numeric nPanjang, nLebar, nLuas
+float Panjang, Lebar, Luas;
+void InputData(){
+    //display "Masukkan Panjang :"
+    cout << "Masukkan Panjang : ";
+    //accept nPanjang
+    cin >> Panjang;
+    //display "Masukkan Lebar :"
+    cout << "Masukkan Lebar : ";
+    //accept nLebar
+    cin >> Lebar;
+}
+float HitungLuas(){
+    //compute nLuas = nPanjang * nLebar
+    return Panjang * Lebar;
+}
+void OutputData(){
+    //display "Luas Persegi Panjang : " + nLuas 
+    cout << "Luas Persegi Panjang : " << HitungLuas(); 
+}
+int main()
+{ //begin
+    InputData();
+    OutputData();
+} //end
